f987765f757bc4814f96bfdf4c1adffee0f21803
Gowa2017 <33367355+Gowa2017@users.noreply.github.com>
2025-02-21 23:35:27 +0800

    refactor: refactor

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"flag"
 	"fmt"
 	"log"
+	"os"
 	"os/exec"
 	"strings"
 )
@@ -13,16 +14,20 @@ var typeList string
 
 func init() {
 	types = make(map[string]string)
-	types["feat"] = "新功能"
-	types["fix"] = "修复 bug"
-	types["docs"] = "文档注释"
-	types["style"] = "代码格式化（不影响运行时变动）"
-	types["refactor"] = "重够（即不增加新功能，也不是修复 Bug）"
-	types["perf"] = "性能优化"
-	types["test"] = "增加测试"
-	types["chore"] = "构建过程或辅助工具变动，如CICD修改，库升级"
-	types["revert"] = "回退"
-	types["build"] = "打包"
+	types["feat"] = "增加新功能"
+	types["fix"] = "修复问题/BUG"
+	types["style"] = "代码风格相关无影响运行结果的"
+	types["perf"] = "优化/性能提升"
+	types["refactor"] = "重构"
+	types["revert"] = "撤销修改"
+	types["test"] = "测试相关"
+	types["docs"] = "文档/注释"
+	types["chore"] = "依赖更新/脚手架配置修改等"
+	types["workflow"] = "工作流改进"
+	types["ci"] = "持续集成"
+	types["mod"] = "不确定分类的修改"
+	types["wip"] = "开发中"
+	types["types"] = "类型修改"
 
 	sl := make([]string, 0)
 	for k, v := range types {
@@ -34,30 +39,31 @@ func init() {
 
 func main() {
 	var scope = flag.String("s", "", "影响范围，如：认证模块、学员模块、公共配置。不是必填，但是建议都进行描述")
-	var changeType = flag.String("t", "fix", fmt.Sprintf("变更类型，可选值:\n%s\n", typeList))
 	flag.Parse()
-
-	if _, ok := types[*changeType]; !ok {
-		fmt.Printf("changeType must be in: %s\n", typeList)
-		return
-	}
-	if flag.NArg() < 1 {
-		fmt.Println("必须指定提交说明，如： gcm 修改用户认证时无法登录的问题")
+	if flag.NArg() < 2 {
+		fmt.Printf("Usage: %s <变更类型> 提交消息 ...其他参数 \n变更类型: \n%s\n", os.Args[0], typeList)
 		return
 	}
 
-	var args []string
-	args = append(args, "commit", "-m")
+	changeType := flag.Arg(0)
+
+	if _, ok := types[changeType]; !ok {
+		fmt.Printf("变更类型必须是以下类型之一:\n%s\n", typeList)
+		return
+	}
+	var cmd []string
+	cmd = append(cmd, "commit", "-m")
 	if *scope != "" {
-		args = append(args, fmt.Sprintf("%s(%s): %s", *changeType, *scope, flag.Arg(0)))
+		cmd = append(cmd, fmt.Sprintf("%s(%s): %s", changeType, *scope, flag.Arg(0)))
 	} else {
-		args = append(args, fmt.Sprintf("%s: %s", *changeType, flag.Args()[0]))
+		cmd = append(cmd, fmt.Sprintf("%s: %s", changeType, flag.Args()[0]))
 	}
 
-	if flag.NArg() > 1 {
-		args = append(args, flag.Args()[1:]...)
+	if flag.NArg() > 2 {
+		cmd = append(cmd, flag.Args()[2:]...)
 	}
-	if ret, err := exec.Command("git", args...).CombinedOutput(); err != nil {
+	fmt.Println(strings.Join(cmd, ""))
+	if ret, err := exec.Command("git", cmd...).CombinedOutput(); err != nil {
 		fmt.Printf("%s\n", ret)
 		log.Fatal(err)
 	}
